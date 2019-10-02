24fc6ea91b4ece012e32a24a8e9e4aa96a8fb189
Nancy4510 <nancy4510@gmail.com>
2019-10-02 16:33:22 -0400

    SEI: Tic-Tac-Toe Update 10/02/19
    I was able to figure out how to display the messages like whose turn
    was next and who has won one by one. I had
    `$( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")` so I
    just changed .append to .html and it worked. When I had .append, it kept
    adding messages and making them stack one on top of the other, where as
    with .html the way I understand it, it is replacing my empty `p` tag
    with 'Player X Won! Congrats!' and showing the messages one by one.

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -39,79 +39,78 @@ $('.box').on('click', function (event)
   {
     $(event.target).text(player)
     player = 'O'
-    $( "#gameMessage" ).append( "<p>It's O's turn to move now!</p>")
-    
+    $( "#gameMessage" ).html( "<p>It's O's turn to move now!</p>")
   }
   else if(player === 'O')
   {
     $(event.target).text(player)
     player = 'X'
-    $( "#gameMessage" ).append( "<p>It's X's turn to move now!</p>")
+    $( "#gameMessage" ).html( "<p>It's X's turn to move now!</p>")
   }
 }
 else
 {
-  $("#gameMessage" ).append( "<p>This square is already occupied! Please choose another one.</p>")
+  $("#gameMessage" ).html( "<p>This square is already occupied! Please choose another one.</p>")
 }
 ///////////CHECKING TO SEE IF PLAYER X WON///////////////////
 //checking to see if the squares are equal to determine a win
 //reading what's in the class `square1, `sqaure2`, `square3` and seeing if they're all equal to X
 if ($('#square1').html() === 'X' && $('#square2').html() === 'X' && $('#square3').html() === 'X'){
   //displaying the message that Player X has won.
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   //turns off the click, preventing the user from clicking after winning
   $('.box').off('click')
 }else if ($('#square4').html() === 'X' && $('#square5').html() === 'X' && $('#square6').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 }
 else if ($('#square7').html() === 'X' && $('#square8').html() === 'X' && $('#square9').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 }else if ($('#square1').html() === 'X' && $('#square4').html() === 'X' && $('#square7').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 } else if ($('#square2').html() === 'X' && $('#square5').html() === 'X' && $('#square8').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 } else if ($('#square3').html() === 'X' && $('#square6').html() === 'X' && $('#square9').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 }else if ($('#square1').html() === 'X' && $('#square5').html() === 'X' && $('#square9').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 }
 else if ($('#square3').html() === 'X' && $('#square5').html() === 'X' && $('#square7').html() === 'X'){
-  $( "#gameMessage" ).append( "<p>Player X Won! Congrats!</p>")
+  $( "#gameMessage" ).html( "<p>Player X Won! Congrats!</p>")
   $('.box').off('click')
 }
 
 ///////////CHECKING TO SEE IF PLAYER O WON///////////////////
 if ($('#square1').html() === 'O' && $('#square2').html() === 'O' && $('#square3').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   //turns off the click, preventing the user from clicking after winning
   $('.box').off('click')
 }else if ($('#square4').html() === 'O' && $('#square5').html() === 'O' && $('#square6').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 }
 else if ($('#square7').html() === 'O' && $('#square8').html() === 'O' && $('#square9').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 }else if ($('#square1').html() === 'O' && $('#square4').html() === 'O' && $('#square7').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 } else if ($('#square2').html() === 'O' && $('#square5').html() === 'O' && $('#square8').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 } else if ($('#square3').html() === 'O' && $('#square6').html() === 'O' && $('#square9').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 }else if ($('#square1').html() === 'O' && $('#square5').html() === 'O' && $('#square9').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
 }else if ($('#square3').html() === 'O' && $('#square5').html() === 'O' && $('#square7').html() === 'O'){
-  $( "#gameMessage" ).append("<p>Player O Won! Congrats!</p>")
+  $( "#gameMessage" ).html("<p>Player O Won! Congrats!</p>")
   $('.box').off('click')
   
 // }else if (){
